9be6527b16fe29045f9ffbbe73b3ceda2aed5ca5
Elias Oliveira <84356504+OliveiraElias@users.noreply.github.com>
2022-09-14 19:28:24 -0300

    Add files via upload

--- page_02.js
+++ page_02.js
@@ -7,15 +7,24 @@ function checkName() {
         
         var nextPage = document.querySelector('#nextPage');
         nextPage.style.display = "inline";
-        alert(theName);
         var hide = document.querySelector('.hide');
         hide.style.display = "none";
+        alert("É isso aí ^^");       
 
         var text = document.querySelector('.texto');
         text.style.display = "none";
         
+        
+    }
+    else if(theName == "LAUDECY")
+    {
+        alert("Ele é muito importante mas ainda não é o mais importante.")
+    }
+    else if(theName == "ELIAS")
+    {
+        alert("Ihhh to longe de ser a pessoa mais importante, tente outra vez.")
     }
     else{
-        alert("Errou! Tente novamente.");
+        alert("Ihhh errou! Tente novamente.");
     }
 }
